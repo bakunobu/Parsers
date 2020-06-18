a6545cf71019e880e3f765a9a5fc2b3bd8a7b9ea
Sergei Vakunov <bakunobu@gmail.com>
2020-06-18 15:11:27 +0300

    doc info added

--- pdf_parser_agro.py
+++ pdf_parser_agro.py
@@ -24,11 +24,97 @@ def show_num_pages(FILE):
     
     Returns:
     ========
-    
-    
+    num_pages: int
+    the number of pages in a given pdf-file
     """
+    
     pdf_file_obj = open(FILE, 'rb')
     pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
     return(type(pdf_reader.numPages))
 
-print(show_num_pages(FILE))
+# testing
+# print(show_num_pages(FILE))
+"""
+Doesn't work - returns an empty page
+pdf_file_obj = open(FILE, 'rb')
+pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
+page_obj = pdf_reader.getPage(22)
+print(page_obj.extractText())
+"""
+
+# tabula-py
+import tabula
+import pandas as pd
+
+
+out_file = 'test.csv'
+start_page = 12
+end_page = 21
+
+
+columns = ['indicator',
+           'all_2006',
+           'all_2016',
+           'companies_2006',
+           'companies_2016',
+           'farmers_and_IE_2006',
+           'farmers_and_IE_2016', 
+           'farmers_2006',
+           'farmers_2016',
+           'IE_2006',
+           'IE_2016',
+           'personal_2006',
+           'personal_2016',
+           'non_profit_2006',
+           'non_profit_2016']
+
+
+def pdf_to_csv_conv(params_dict):
+    
+    '''
+    A parser that converts a pdf table to a csv file
+    Adjusted for table 1 ('Main Results')
+    
+    Args:
+    =====
+    params_dict: dict
+        
+    Contains:
+    =========
+    PDF_FILE: str
+    a filename or a path to a file
+    
+    start_page: str
+    the first page to be converted (if only one page number
+    is given, converts the exact page)
+    
+    end_page: str (optional)
+    the last page to be converted
+    
+    output_file: str (optional)
+    a filename or a path to a file  the information
+    should be written to
+    a default value is 'test.csv' in the same folder
+    
+    COLUMNS: list or tuple
+    a list of column names
+    
+    Returns:
+    None: None type
+    Writes the data to the specified .csv file
+    '''
+    
+    PDF_FILE = params_dict.get('PDF_FILE')
+    start_page = params_dict.get('start_page')
+    end_page = params_dict.get('end_page', '')
+    output_file = params_dict.get('output_file', 'test.csv')
+    COLUMNS = params_dict.get('COLUMNS')
+    
+    
+    df = pd.DataFrame(columns=COLUMNS)
+
+
+tabula.convert_into(FILE, 'test.csv', pages='12-21', 
+                     stream=True, output_format='csv')
+
+with open
