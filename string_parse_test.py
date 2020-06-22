95ab4e97f226338f545214c78a3cd9a5dcdd84be
Sergei Vakunov <bakunobu@gmail.com>
2020-06-22 13:59:14 +0300

    case three added

--- string_parse_test.py
+++ string_parse_test.py
@@ -1,14 +1,14 @@
 import numpy as np
 
 
-test_string = '"Число организаций (хозяйств) - всего, тыс.",х х,,"59,2 36,0","285,1 174,8","253,1 136,7","32,0 38,0","22799,4 23496,9","80,3 75,9"'
+test_string = 'категории организаций (хозяйств),х х,,"68,6 76,3","51,7 66,1","49,9 65,9","66,5 66,9","88,7 79,8","92,7 89,0"'
+
 def string_parser(test_string):
-    
+    preprocessed = []
     if test_string[:2] == '""':
         print('PASS!')
     
     elif test_string[:1] == '"':
-        preprocessed = []
         test_string = test_string[1:].split('"')
         preprocessed = [test_string[0]]
         for el in test_string[1:-1]:
@@ -31,6 +31,35 @@ def string_parser(test_string):
                         preprocessed.append(x)
                     else:
                         pass
-        print(preprocessed)
+    
+    elif '"' in test_string:
+        if ',х х,,' in test_string:
+            test_string = test_string.replace(',х х,,', '",х х,,"')
+        test_string = test_string.split('"')
+        preprocessed = [test_string[0]]
+        test_string = [x for x in test_string if len(x) > 1]
+        print(test_string)
+        for el in test_string[1:-1]:
+            if el == ',':
+                test_string.remove(',')
+            if el == '\n':
+                test_string.remove('\n')
+            if el == '':
+                test_string.remove('')
+            el = el.replace(',', '.')
+            el = el.split(' ')
+            for x in el:
+                    # print(el)
+                try:
+                    x = float(x)
+                    preprocessed.append(x)
+                except:
+                    if el != '.' or el != '':
+                        x = np.NaN
+                        preprocessed.append(x)
+                    else:
+                        pass
+    
+    print(preprocessed)
 
 string_parser(test_string)
